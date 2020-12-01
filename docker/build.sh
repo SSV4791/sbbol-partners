0dc5cb3752e7eec47bba041941887a71855df08d
Демаков Сергей Александрович <Demakov.S.Al@sberbank.ru>
2020-12-01 00:11:02 -1000

    LORIS-64 Развернуть фабрику партнеров на OpenShift Поправил настройки еобходимые для сборки и деплоя

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-docker rmi registry.sigma.sbrf.ru/pprb-dev/ci00682829_cdm_dev/dataspace-deposit-python:latest
-docker build -t registry.sigma.sbrf.ru/pprb-dev/ci00682829_cdm_dev/dataspace-deposit-python:latest ./
