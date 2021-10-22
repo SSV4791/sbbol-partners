b47a702a57a9bc8e9cf0e6b4ab0387d9e267ba7f
Осипов Артем Олегович <Osipov.A.Ole@sberbank.ru>
2021-10-22 13:12:43 +0300

    DCBBRAIN-1436 Добавлен guid проекта для публикацию в META

--- buildSrc/src/main/kotlin/meta-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/meta-conventions.gradle.kts
@@ -13,7 +13,7 @@ meta {
     nexusUrl = null
     nexusUser = nexusLoginValue
     nexusPassword = nexusPasswordValue
-    componentId = "APP744"
+    componentId = "9655c0f1-74bf-11eb-6742-005056b72594"
     ext {
         set("url", "https://meta.sigma.sbrf.ru")
         set("openApiSpecs", listOf("/partners-openapi/openapi/renters/renter.yaml"))
