2f49eba1c4ddebba63d3a6ee6da7967e87a21784
Осипов Артем Олегович <Osipov.A.Ole@sberbank.ru>
2022-07-20 16:27:30 +0300

    DCBBRAIN-2850 Добавить в выгрузку yml партнеров и контрагентов в МЕТА

--- buildSrc/src/main/kotlin/meta-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/meta-conventions.gradle.kts
@@ -19,7 +19,25 @@ meta {
         set(
             "openApiSpecs",
             listOf(
-                "${project(":partners-openapi").projectDir}/openapi/renters/renter.yaml"
+                //Renter
+                "${project(":partners-openapi").projectDir}/openapi/renters/renter.yaml",
+                //Partner
+                "${project(":partners-openapi").projectDir}/openapi/partner/account.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/account_sign.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/contact.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/contact_address.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/contact_document.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/contact_email.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/contact_phone.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/dictionary_budget_mask.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/dictionary_document.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/partner.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/partner_address.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/partner_document.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/partner_email.yaml",
+                "${project(":partners-openapi").projectDir}/openapi/partner/partner_phone.yaml",
+                //Counterparty
+                "${project(":partners-openapi").projectDir}/openapi/counterparties/counterparty.yaml",
             )
         )
         set("analyzeJava", false)
