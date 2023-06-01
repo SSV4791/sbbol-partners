b2bb97b23675b876838c515a7bccb1368d04ab8b
Щеглов Сергей Викторович <shcheglov.s.vik@sberbank.ru>
2023-06-01 13:33:34 +0300

    DCBBRAIN-4494 Добавил механизм retry для реста \counterparty\check-requisites

--- partners-adapter/src/main/java/ru/sberbank/pprb/sbbol/partners/legacy/config/PartnerAdapterConfiguration.java
+++ partners-adapter/src/main/java/ru/sberbank/pprb/sbbol/partners/legacy/config/PartnerAdapterConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.sberbank.pprb.sbbol.partners.legacy.LegacySbbolAdapter;
@@ -56,7 +58,20 @@ public class PartnerAdapterConfiguration {
     }
 
     @Bean
-    public LegacySbbolAdapter legacySbbolAdapter(RestTemplate restTemplate) {
-        return new LegacySbbolAdapterImpl(restTemplate);
+    public RetryTemplate sbbolLegacyRetryTemplate(
+        @Value("${sbbol.retry.max_attempts:3}") Integer maxAttempts,
+        @Value("${sbbol.retry.interval:5000}") Long retryTimeInterval
+    ) {
+        return RetryTemplate.builder()
+            .notRetryOn(HttpClientErrorException.BadRequest.class)
+            .traversingCauses()
+            .maxAttempts(maxAttempts)
+            .fixedBackoff(retryTimeInterval)
+            .build();
+    }
+
+    @Bean
+    public LegacySbbolAdapter legacySbbolAdapter(RestTemplate restTemplate, RetryTemplate sbbolLegacyRetryTemplate) {
+        return new LegacySbbolAdapterImpl(restTemplate, sbbolLegacyRetryTemplate);
     }
 }
