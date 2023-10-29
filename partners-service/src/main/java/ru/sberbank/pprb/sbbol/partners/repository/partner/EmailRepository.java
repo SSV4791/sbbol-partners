7461afc476df8dfa087b204ac618ffe0c1c19b6a
Осипов Артем Олегович <osipov.a.ole@sberbank.ru>
2023-10-29 15:28:14 +0300

    DCBBRAIN-5162 Удаление методов Transactional где они не нужны

--- partners-service/src/main/java/ru/sberbank/pprb/sbbol/partners/repository/partner/EmailRepository.java
+++ partners-service/src/main/java/ru/sberbank/pprb/sbbol/partners/repository/partner/EmailRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import ru.sberbank.pprb.sbbol.partners.entity.partner.EmailEntity;
 import ru.sberbank.pprb.sbbol.partners.repository.partner.common.EmailViewRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,13 +19,4 @@ public interface EmailRepository extends CrudRepository<EmailEntity, UUID>, Emai
      * @return Электронный адрес
      */
     Optional<EmailEntity> getByDigitalIdAndUuid(String digitalId, UUID uuid);
-
-    /**
-     * Получение электронного адреса
-     *
-     * @param digitalId   Идентификатор личного кабинета
-     * @param unifiedUuid Идентификатор партнера/контакта
-     * @return Электронные адреса
-     */
-    List<EmailEntity> findByDigitalIdAndUnifiedUuid(String digitalId, UUID unifiedUuid);
 }
