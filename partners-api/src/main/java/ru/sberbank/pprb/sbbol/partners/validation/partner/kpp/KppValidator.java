dec243511ef16ece16dd52bb8d8d53c273a14f96
Рузин Кирилл Иванович <KIRuzin@sberbank.ru>
2023-12-27 13:56:45 +0300

    DCBBRAIN-5734 - Обновление маски валидации КПП

--- partners-api/src/main/java/ru/sberbank/pprb/sbbol/partners/validation/partner/kpp/KppValidator.java
+++ partners-api/src/main/java/ru/sberbank/pprb/sbbol/partners/validation/partner/kpp/KppValidator.java
@@ -6,11 +6,12 @@ import ru.sberbank.pprb.sbbol.partners.validation.common.BaseValidator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 public class KppValidator extends BaseValidator
     implements ConstraintValidator<KppValidation, String> {
 
-    private static final String PATTERN = "[0-9a-zA-Zа-яА-ЯЁё№! \"#$%&'()*+,-./:;<=>?@\\^_`{|}~\n\r]{9}";
+    private static final Pattern PATTERN = Pattern.compile("[0-9a-zA-Zа-яА-ЯЁё№! \"#$%&'()*+,-./:;<=>?@\\^_`{|}~\n\r]{9}");
     private static final String MESSAGE = "{validation.partner.kpp.format}";
 
     @Override
@@ -19,6 +20,6 @@ public class KppValidator extends BaseValidator
             return true;
         }
         buildMessage(context, MESSAGE);
-        return value.matches(PATTERN);
+        return PATTERN.matcher(value).matches();
     }
 }
